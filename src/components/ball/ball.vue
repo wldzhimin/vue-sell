7643c1f6a78d1214e7904a1a663289b904b0ddfb
wldzhimin <Zhimin.Liu@wolaidai.com>
2017-02-08 11:11:21 +0800

    store init

--- src/components/ball/ball.vue
+++ src/components/ball/ball.vue
@@ -78,18 +78,5 @@
 </script>
 
 <style lang="stylus" scoped>
-.ball-container
-  .ball
-    position: fixed
-    left: 32px
-    bottom: 22px
-    z-index: 200
-    &.drop-transition
-      transition: all 0.4s cubic-bezier(0.49, -0.29, 0.75, 0.41)
-      .inner
-        width: 16px
-        height: 16px
-        border-radius: 50%
-        background: rgb(0, 160, 220)
-        transition: all 0.4s linear
+  @import 'ball.styl';
 </style>
--- src/main.js
+++ src/main.js
@@ -1,17 +1,16 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Vuex from 'vuex';
 import VueResource from 'vue-resource';
 import App from './App';
 import goods from 'components/goods/goods';
 import ratings from 'components/ratings/ratings';
 import seller from 'components/seller/seller';
+import store from './store'
 
 import 'common/stylus/index.styl';
 
 Vue.use(VueRouter);
 Vue.use(VueResource);
-Vue.use(Vuex);
 
 let router = new VueRouter({
   linkActiveClass: 'active',
